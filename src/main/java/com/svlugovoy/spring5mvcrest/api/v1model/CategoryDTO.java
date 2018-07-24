5c7925478cdac63a190f6e73876b111b236665d4
svlugovoy <svlugovoy@gmail.com>
2018-07-24 09:20:02 +0300

    Application development started

---
+++ src/main/java/com/svlugovoy/spring5mvcrest/api/v1model/CategoryDTO.java
@@ -0,0 +1,8 @@
+package com.svlugovoy.spring5mvcrest.api.v1model;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/com/svlugovoy/spring5mvcrest/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.svlugovoy.spring5mvcrest.repository;
+
+import com.svlugovoy.spring5mvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
