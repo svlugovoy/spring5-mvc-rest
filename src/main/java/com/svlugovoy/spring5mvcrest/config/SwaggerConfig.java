045094012da1da49a5a7610bd8b26ea5640f8d4e
svlugovoy <svlugovoy@gmail.com>
2018-07-25 12:33:13 +0300

    Customized swagger mete data

--- src/main/java/com/svlugovoy/spring5mvcrest/config/SwaggerConfig.java
+++ src/main/java/com/svlugovoy/spring5mvcrest/config/SwaggerConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig { //} extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api(){
@@ -19,7 +23,35 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
+
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Serhii Luhovyi", "https://google.com",
+                "test@test.com");
+
+        return new ApiInfo(
+                "Rest demo app",
+                "Some description",
+                "1.0",
+                "Terms of Service: blah",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
+
+// for non spring-boot projects
+
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 
 }
